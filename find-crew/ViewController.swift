3e5d697494f3151562cb2a9447cf15d8307dbe1d
iyinraphael <iyinraphael@gmail.com>
2020-05-05 09:18:41 -0700

    initiaaaaaaaaal set up

---
+++ find-crew/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  find-crew
+//
+//  Created by Iyin Raphael on 5/5/20.
+//  Copyright © 2020 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
